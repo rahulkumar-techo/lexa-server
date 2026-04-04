e227193276c0b29046808335b1f5a4273eb4c8c5
Rahul Kumar <rahulkumar7250883289@gmail.com>
2026-04-04 18:48:16 +0530

    feat: add scenario repository

---
+++ src/modules/scenario/scenario.repo.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/src/lib/prisma";
+import { Prisma, Scenario } from "@/generated/prisma/client";
+
+class ScenarioRepo {
+  async getScenarios(): Promise<Scenario[]> {
+    return prisma.scenario.findMany({
+      orderBy: {
+        created_at: "desc"
+      }
+    });
+  }
+
+  async getScenarioById(id: string): Promise<Scenario | null> {
+    return prisma.scenario.findUnique({
+      where: { id }
+    });
+  }
+
+  async createScenario(data: Prisma.ScenarioCreateInput): Promise<Scenario> {
+    return prisma.scenario.create({
+      data
+    });
+  }
+}
+
+export { ScenarioRepo };
+export default new ScenarioRepo();
