dfde9a7c5e0c47730d5182ea59dd43fb397a9176
Rahul Kumar <rahulkumar7250883289@gmail.com>
2026-04-04 18:49:56 +0530

    feat: add groq ai configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,7 @@ type NodeEnvironment = "development" | "test" | "production";
 
 export const env = {
   NODE_ENV: (process.env.NODE_ENV ?? "development") as NodeEnvironment,
-  PORT: Number(process.env.PORT ?? 5000)
+  PORT: Number(process.env.PORT ?? 5000),
+  groq_api_key: String(process.env.GROQ_API_KEY),
+  GROQ_MODEL: process.env.GROQ_MODEL
 };
